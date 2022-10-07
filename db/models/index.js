2761c4b9ec8e9525a802dd16c281d031c2452b69
Jefferson Ortiz R <jortizr@ut.edu.co>
2022-10-07 02:21:04 -0500

    creacion de la estructura customer para la db

--- db/models/index.js
+++ db/models/index.js
@@ -1,8 +1,11 @@
 const { User, UserSchema } = require("./user.model");
+const { Customer, CustomerSchema } = require("./customer.model");
 
 function setupModels(sequelize) {
   //el metodo init tiene que tener ese schema del user
   //se envia el metodo estatico q no require de invocacion
   User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+  Customer.associate(sequelize.models);
 }
 module.exports = setupModels;
---
+++ services/customer.service.js
@@ -0,0 +1,38 @@
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
+class CustomerService {
+  constructor() {}
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async find() {
+    const customers = await models.Customer.findAll();
+    return customers;
+  }
+
+  async findOne(id) {
+    const customer = await models.Customer.findByPk(id);
+    if (!customer) {
+      throw boom.notFound('user not found');
+    }
+    return { customer };
+  }
+
+  async update(id, changes) {
+    const customerUpdate = await this.findOne(id);
+    await models.Customer.update(changes, { where: { id } });
+    return customerUpdate;
+  }
+
+  async delete(id) {
+    const deleteCustomer = await this.findOne(id);
+    await models.Customer.destroy({ where: { id } });
+    return deleteCustomer;
+  }
+}
+
+module.exports = CustomerService;
