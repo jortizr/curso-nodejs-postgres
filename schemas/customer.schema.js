2798fe1d9dad4a3ae391b6aef38bd0aa9a5bb1f6
Jefferson O <jortizr@ut.edu.co>
2022-10-09 22:23:38 -0500

    integracion de create customer y user juntos

--- schemas/customer.schema.js
+++ schemas/customer.schema.js
@@ -4,13 +4,18 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3).max(30);
 const lastName = Joi.string().min(3).max(30);
 const phone = Joi.string().min(7);
+const email = Joi.string().email();
+const password = Joi.string().min(8);
 const userId = Joi.number().integer();
 
 const createCustomerSchema = Joi.object({
   name: name.required(),
   lastName: lastName.required(),
   phone: phone.required(),
-  userId: userId.required()
+  user: Joi.object({
+    email: email.required(),
+    password: password.required()
+  })
 });
 
 const updateCustomerSchema = Joi.object({
--- services/customer.service.js
+++ services/customer.service.js
@@ -5,7 +5,11 @@ class CustomerService {
   constructor() {}
 
   async create(data) {
-    const newCustomer = await models.Customer.create(data);
+    const newUser = await models.User.create(data.user);
+    const newCustomer = await models.Customer.create({
+      ...data,
+      userId: newUser.id
+    });
     return newCustomer;
   }
 
