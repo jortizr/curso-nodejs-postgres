318b97f49e796b1a59dbeb1d94b7ab75c8e4e5c6
Jefferson O <jortizr@ut.edu.co>
2022-10-10 00:38:40 -0500

    creacion modelos de prod y categ y migracion

--- db/models/index.js
+++ db/models/index.js
@@ -1,5 +1,8 @@
 const { User, UserSchema } = require("./user.model");
 const { Customer, CustomerSchema } = require("./customer.model");
+//cargamos los modelos
+const { Product, ProductSchema } = require("./product.model");
+const { Category, CategorySchema } = require("./category.model");
 
 function setupModels(sequelize) {
   //el metodo init tiene que tener ese schema del user
@@ -7,7 +10,13 @@ function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
 
+  //agregamos la inicializacion de los modelos
+  Product.init(ProductSchema, Product.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize));
+
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
+  Category.associate(sequelize.models);
+  Product.associate(sequelize.models);
 }
 module.exports = setupModels;
